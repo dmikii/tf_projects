0891ae12acecf94e17bb88abbea38e2e5a19b32e
dmikii <kddiidmk@gmail.com>
2024-07-12 08:45:30 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-07d9b9ddc6cd8dd30"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
+
+    user_data_replace_on_change = true
+
+    tags = {
+        Name = "webserver"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
